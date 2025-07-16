36375a083b9817a90a14acc2c88496a0ca79ffb2
Mirazamn <mirsaidovmirazam@gmail.com>
2025-07-16 09:36:25 +0500

    Commit

--- Server/routes/posts.js
+++ Server/routes/posts.js
@@ -43,6 +43,104 @@ router.get('/:id/comments', (req,res) => {
     })
 })
 
+// New comment
+router.post('/:id/comments', (req, res) => {
+    fs.readFile(Path, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error reading file!');
+        }
+
+        const posts = JSON.parse(data);
+        const postIndex = posts.findIndex(p => Number(p.id) === Number(req.params.id));
+
+        if (postIndex === -1) {
+            return res.status(404).send("Error post not found!");
+        }
+
+        // 👇 comments mavjudligini tekshiramiz
+        if (!Array.isArray(posts[postIndex].comments)) {
+            posts[postIndex].comments = [];
+        }
+
+        // 👇 req.body ni tekshiramiz
+        const { value, username, id } = req.body;
+        if (!value || !username || typeof id !== 'number') {
+            return res.status(400).send("Invalid comment data!");
+        }
+
+        posts[postIndex].comments.push({ value, username, id });
+
+        fs.writeFile(Path, JSON.stringify(posts, null, 2), (err) => {
+            if (err) {
+                return res.status(500).send('Error writing file!');
+            }
+            console.log('Comment added');
+            res.json({ message: "Comment added successfully!" });
+        });
+    });
+});
+
+// Get comment by id
+router.get('/:id/comments/:cid', (req,res) => {
+    fs.readFile(Path, 'utf8', (err,data) => {
+        if (err) {
+            return res.status(500).send('Error in reading file!')
+        }
+        const posts = JSON.parse(data)
+        const post = posts.find(p => Number(p.id) == Number(req.params.id))
+        const comments = post.comments
+        const comment = comments.find(c => Number(c.id) === Number(req.params.cid))
+        res.json(comment)
+    })
+})
+
+// Delete Comment
+router.delete('/:id/comments/:cid', (req, res) => {
+    fs.readFile(Path, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send("Error reading file!");
+        }
+
+        const posts = JSON.parse(data);
+        const postIndex = posts.findIndex(p => Number(p.id) === Number(req.params.id));
+
+        if (postIndex === -1) {
+            return res.status(404).send("Post not found!");
+        }
+
+        const oldComments = posts[postIndex].comments || [];
+        const newComments = oldComments.filter(c => Number(c.id) !== Number(req.params.cid));
+        posts[postIndex].comments = newComments;
+
+        fs.writeFile(Path, JSON.stringify(posts, null, 2), (err) => {
+            if (err) {
+                return res.status(500).send("Error writing file!");
+            }
+            console.log("Comment deleted");
+            res.json({ message: "Comment deleted successfully!" });
+        });
+    });
+});
+
+// PUT Comment
+router.put('/:id/comments/:cid', (req,res) => {
+    fs.readFile(Path,'utf8',(err,data) => {
+        if (err) {
+            return res.status(500).send("Error reading file")
+        }
+        const posts = JSON.parse(data)
+        const post = posts.find(p => Number(p.id) == Number(req.params.id))
+        const postIndex = posts.findIndex(p => Number(p.id) == Number(req.params.id))
+        if (postIndex === -1) {
+            return res.status(404).send("Post not found!")
+        }
+        const comments = post.comments
+        const commentIndex = comments.findIndex(c => Number(c.id) == Number(req.params.cid))
+        comments[commentIndex] = {...comments[commentIndex], ...req.body}
+        fs.writeFile(Path,JSON.stringify())
+    })
+})
+
 // Delete post
 router.delete('/:id', (req,res) => {
     fs.readFile(Path, 'utf8', (err,data) => {
